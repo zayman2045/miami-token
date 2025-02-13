d7665683eb77bcc783f334195dde49f5f1f62d91
zayman2045 <xavierorlandogriffith@gmail.com>
2025-02-13 08:36:38 -0500

    feat: implement airdrop_tokens function and update minting logic

--- anchor/programs/miamitoken/src/lib.rs
+++ anchor/programs/miamitoken/src/lib.rs
@@ -1,7 +1,11 @@
 #![allow(clippy::result_large_err)]
 
 use anchor_lang::prelude::*;
-use anchor_spl::token_interface::{Mint, TokenInterface};
+use anchor_spl::{
+    token_2022::{mint_to, MintTo},
+    associated_token::AssociatedToken,
+    token_interface::{Mint, TokenAccount, TokenInterface},
+};
 
 declare_id!("coUnmi3oBUtwtd9fjeAvSsJssXh5A5xyPbhpewyzRVF");
 
@@ -10,8 +14,34 @@ pub mod miami_token {
     use super::*;
 
     pub fn create_token_mint(ctx: Context<CreateMintAccount>) -> Result<()> {
-      msg!("Token mint created successfully! The mint address is: {}", ctx.accounts.token_mint.key());
-      Ok(())
+        msg!(
+            "Token mint created successfully! The mint address is: {}",
+            ctx.accounts.token_mint.key()
+        );
+        Ok(())
+    }
+
+    pub fn airdrop_tokens(ctx: Context<AirdropTokens>, amount: u64) -> Result<()> {
+        let signer_seeds: &[&[&[u8]]] = &[&[b"mint", &[ctx.bumps.token_mint]]];
+
+        let cpi_accounts = MintTo {
+            mint: ctx.accounts.token_mint.to_account_info(),
+            to: ctx.accounts.token_account.to_account_info(),
+            authority: ctx.accounts.token_mint.to_account_info(),
+        };
+
+        let cpi_program = ctx.accounts.token_program.to_account_info();
+
+        let cpi_ctx = CpiContext::new_with_signer(cpi_program, cpi_accounts, signer_seeds);
+
+        mint_to(cpi_ctx, amount)?;
+
+        msg!(
+            "Airdropped {} tokens to {}",
+            amount,
+            ctx.accounts.user.key()
+        );
+        Ok(())
     }
 }
 
@@ -24,9 +54,29 @@ pub struct CreateMintAccount<'info> {
       payer = signer,
       mint::authority = token_mint,
       mint::decimals = 9,
+      seeds = [b"mint"],
+      bump
     )]
     pub token_mint: InterfaceAccount<'info, Mint>,
     pub system_program: Program<'info, System>,
     pub token_program: Interface<'info, TokenInterface>,
 }
 
+#[derive(Accounts)]
+pub struct AirdropTokens<'info> {
+    #[account(mut)]
+    pub user: Signer<'info>,
+    #[account(mut, seeds = [b"mint"], bump)]
+    pub token_mint: InterfaceAccount<'info, Mint>,
+    #[account(
+    init_if_needed,
+    payer = user,
+    associated_token::mint = token_mint,
+    associated_token::authority = user,
+    associated_token::token_program = token_program
+  )]
+    pub token_account: InterfaceAccount<'info, TokenAccount>,
+    pub token_program: Interface<'info, TokenInterface>,
+    pub system_program: Program<'info, System>,
+    pub associated_token_program: Program<'info, AssociatedToken>,
+}
