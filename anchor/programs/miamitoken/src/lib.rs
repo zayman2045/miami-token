239643cb2056562efe60a26db19d85280203780f
zayman2045 <xavierorlandogriffith@gmail.com>
2025-02-13 08:07:25 -0500

    fix: rename miamitoken module to miami_token and implement create_token_mint function

--- anchor/programs/miamitoken/src/lib.rs
+++ anchor/programs/miamitoken/src/lib.rs
@@ -1,70 +1,32 @@
 #![allow(clippy::result_large_err)]
 
 use anchor_lang::prelude::*;
+use anchor_spl::token_interface::{Mint, TokenInterface};
 
 declare_id!("coUnmi3oBUtwtd9fjeAvSsJssXh5A5xyPbhpewyzRVF");
 
 #[program]
-pub mod miamitoken {
+pub mod miami_token {
     use super::*;
 
-  pub fn close(_ctx: Context<CloseMiamitoken>) -> Result<()> {
-    Ok(())
-  }
-
-  pub fn decrement(ctx: Context<Update>) -> Result<()> {
-    ctx.accounts.miamitoken.count = ctx.accounts.miamitoken.count.checked_sub(1).unwrap();
-    Ok(())
-  }
-
-  pub fn increment(ctx: Context<Update>) -> Result<()> {
-    ctx.accounts.miamitoken.count = ctx.accounts.miamitoken.count.checked_add(1).unwrap();
-    Ok(())
-  }
-
-  pub fn initialize(_ctx: Context<InitializeMiamitoken>) -> Result<()> {
-    Ok(())
-  }
-
-  pub fn set(ctx: Context<Update>, value: u8) -> Result<()> {
-    ctx.accounts.miamitoken.count = value.clone();
-    Ok(())
-  }
+    pub fn create_token_mint(ctx: Context<CreateMintAccount>) -> Result<()> {
+      msg!("Token mint created successfully! The mint address is: {}", ctx.accounts.token_mint.key());
+      Ok(())
+    }
 }
 
 #[derive(Accounts)]
-pub struct InitializeMiamitoken<'info> {
-  #[account(mut)]
-  pub payer: Signer<'info>,
-
-  #[account(
-  init,
-  space = 8 + Miamitoken::INIT_SPACE,
-  payer = payer
-  )]
-  pub miamitoken: Account<'info, Miamitoken>,
-  pub system_program: Program<'info, System>,
-}
-#[derive(Accounts)]
-pub struct CloseMiamitoken<'info> {
-  #[account(mut)]
-  pub payer: Signer<'info>,
-
-  #[account(
-  mut,
-  close = payer, // close account and return lamports to payer
-  )]
-  pub miamitoken: Account<'info, Miamitoken>,
+pub struct CreateMintAccount<'info> {
+    #[account(mut)]
+    pub signer: Signer<'info>,
+    #[account(
+      init,
+      payer = signer,
+      mint::authority = token_mint,
+      mint::decimals = 9,
+    )]
+    pub token_mint: InterfaceAccount<'info, Mint>,
+    pub system_program: Program<'info, System>,
+    pub token_program: Interface<'info, TokenInterface>,
 }
 
-#[derive(Accounts)]
-pub struct Update<'info> {
-  #[account(mut)]
-  pub miamitoken: Account<'info, Miamitoken>,
-}
-
-#[account]
-#[derive(InitSpace)]
-pub struct Miamitoken {
-  count: u8,
-}
