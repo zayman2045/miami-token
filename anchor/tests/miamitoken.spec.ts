07d3155469bd9d48cdb1ce67614ecb6e79d19bb2
zayman2045 <xavierorlandogriffith@gmail.com>
2025-02-14 00:20:48 -0500

    feat: add airdrop functionality and user funding in Miami Token tests

--- anchor/tests/miamitoken.spec.ts
+++ anchor/tests/miamitoken.spec.ts
@@ -1,6 +1,6 @@
 import * as anchor from "@coral-xyz/anchor";
 import { Program } from "@coral-xyz/anchor";
-import { Keypair } from "@solana/web3.js";
+import { Keypair, LAMPORTS_PER_SOL, Transaction } from "@solana/web3.js";
 import { MiamiToken } from "../target/types/miami_token";
 import { TOKEN_PROGRAM_ID } from "@solana/spl-token";
 
@@ -8,6 +8,7 @@ describe("Miami Token", () => {
   // Configure the client to use the local cluster.
   const provider = anchor.AnchorProvider.env();
   anchor.setProvider(provider);
+  const connection = provider.connection;
   const payer = provider.wallet as anchor.Wallet;
 
   const program = anchor.workspace.MiamiToken as Program<MiamiToken>;
@@ -18,7 +19,7 @@ describe("Miami Token", () => {
     program.programId
   );
 
-  console.log("Miami token mint: ", miamiTokenKMint.toBase58());
+  const user = Keypair.generate();
 
   it("Initialize Miami Token", async () => {
     const tx = await program.methods
@@ -29,6 +30,35 @@ describe("Miami Token", () => {
       })
       .rpc({ skipPreflight: true, commitment: "confirmed" });
 
-    console.log("Your transaction signature", tx);
+    console.log("Token mint initialization successful: ", tx);
+  });
+
+  it("Airdrop tokens to user", async () => {
+    // Fund the user with 2 SOL
+    const airdropSolSignature = await provider.connection.requestAirdrop(
+      user.publicKey,
+      2 * LAMPORTS_PER_SOL
+    );
+
+    const latestBlockHash = await provider.connection.getLatestBlockhash();
+
+    await provider.connection.confirmTransaction({
+      blockhash: latestBlockHash.blockhash,
+      lastValidBlockHeight: latestBlockHash.lastValidBlockHeight,
+      signature: airdropSolSignature,
+    }, "confirmed");
+
+    const airdropAmount = new anchor.BN(100);
+
+    const tx = await program.methods
+      .airdropTokens(airdropAmount)
+      .accounts({
+        user: user.publicKey,
+        tokenProgram: TOKEN_PROGRAM_ID,
+      })
+      .signers([user])
+      .rpc({ skipPreflight: false, commitment: "confirmed" });
+
+    console.log("Airdrop successful: ", tx);
   });
 });
